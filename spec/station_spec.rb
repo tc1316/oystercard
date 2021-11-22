2d6e0a4cdf33bf100449f52c1450bfac7bcff3a3
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-11-22 16:48:40 +0000

    Write tests for new Station instance

---
+++ spec/station_spec.rb
@@ -0,0 +1,14 @@
+require 'station'
+require 'oystercard'
+
+describe Station do
+  let(:station) { described_class.new("Aldgate East", 1) }
+
+  it 'has a name' do
+    expect(station.name).to be("Aldgate East")
+  end
+
+  it 'has a zone' do
+    expect(station.zone).to be(1)
+  end
+end
