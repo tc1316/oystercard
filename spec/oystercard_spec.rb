f4537db173e6b5d23348430cddeebeeefe2c897b
Titus Chang <titus@Tituss-MacBook-Air.local>
2021-11-22 14:34:38 +0000

    Oystercard initialize made

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,13 @@
 require 'oystercard'
 
 describe Oystercard do
+  subject(:oystercard) { described_class.new }
+
+
+  it "has default balance of 0" do
+    expect(oystercard.balance).to be(0)
+  end
+
+
 
 end
