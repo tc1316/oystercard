f63381c351001015cb3dfe564927f36a1fb64172
Titus Chang <titus@Tituss-MacBook-Air.local>
2021-11-22 15:22:32 +0000

    Touch in touch out done

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,10 @@
 class Oystercard
   MAX_BALANCE = 90
-  attr_reader :balance
+  attr_reader :balance, :in_journey
 
   def initialize(balance=0)
     @balance = balance
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -13,6 +14,18 @@ class Oystercard
 
   def deduct(fare=3)
     @balance -= fare
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
   end
   
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -15,6 +15,7 @@ describe Oystercard do
     it "cannot be topped up beyond max balance" do
       expect{oystercard.top_up(Oystercard::MAX_BALANCE+0.01)}.to raise_error("Cannot top up beyond #{Oystercard::MAX_BALANCE}")
     end
+  end
 
   context "when paying" do
     it "deducts fare from balance" do
@@ -22,6 +23,22 @@ describe Oystercard do
       expect(oystercard.balance).to be(-3)
     end
   end
+
+  context "when touching in" do
+    it "the card should be identified as undergoing a journey" do
+      oystercard.touch_in
+      expect(oystercard.in_journey?).to be_truthy
+    end
   end
+
+  context "when touching out" do
+    it "the card should be identified as no longer in a journey" do
+      oystercard.touch_in
+      oystercard.touch_out
+      expect(oystercard.in_journey?).to be_falsey
+    end
+  end
+
+
 
 end
